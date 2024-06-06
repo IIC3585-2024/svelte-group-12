2e20edd9b582d1359915993da6f820328fd07925
CristobalPerez-Cotapos <cprezcotapos@uc.cl>
2024-06-06 00:08:46 -0400

    en proceso

--- src/routes/cities/+page.svelte
+++ src/routes/cities/+page.svelte
@@ -1,10 +1,35 @@
 <script>
   import { goto } from '$app/navigation';
   import { fly } from 'svelte/transition';
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
 
   let isFocused = false;
   let searchValue = '';
-  let options = ['Santiago', 'Curico', 'Valparaiso', 'Puerto Varas']; // Tus opciones precargadas
+  let newCity = '';
+  let options = writable(['Santiago', 'Curico', 'Valparaiso', 'Puerto Varas']); // Tus opciones precargadas
+  
+  const fetchCities = async () => {
+    try {
+      const response = await fetch(`http://52.2.71.125:8000/cities/`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch city data');
+      }
+      const cityData = await response.json();
+      console.log(cityData); // Verificar la estructura de los datos
+      if (Array.isArray(cityData)) {
+        options.set(cityData.map((city) => city.name));
+      } else {
+        throw new Error('City data is not an array');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  onMount(async () => {
+    await fetchCities();
+  });
 
   function handleSubmit() {
     goto(`/cities/${searchValue}`);
@@ -14,8 +39,32 @@
     const regex = new RegExp(searchValue, 'ig'); // Crea una regex a partir de searchValue
     return regex.test(option); // Prueba si la opción coincide con la regex
   }
-</script>
 
+  async function addCity() {
+    if (newCity) {
+      try {
+        const response = await fetch('http://52.2.71.125:8000/cities/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ name: newCity,
+                                 image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Cerro_Renca%2C_Santiago.jpg/275px-Cerro_Renca%2C_Santiago.jpg'
+           })
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to add city');
+        }
+
+        options.update(cities => [...cities, newCity]);
+        newCity = ''; // Limpiar la variable después de agregar la ciudad
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+</script>
 
 <div class="main-div">
   <video class:dim={isFocused} id="video" autoPlay muted loop>
@@ -31,7 +80,7 @@
         class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
         on:click={() => isFocused = true}
         on:blur={() => isFocused = false}
-        >
+      >
       <button type="submit" class="ml-3 p-3 bg-blue-800 rounded-full">
         <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
           <path fill="white" d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"/>
@@ -39,7 +88,7 @@
       </button>
     </form>
     <ul class="list-group">
-      {#each options as option (option)}
+      {#each $options as option (option)}
         {#if matchesSearchValue(option) && searchValue !== option && searchValue !== ''}
           <li>
             <button class="list-group-item" value={option} on:click={() => searchValue = option} on:keydown={(event) => {if (event.key === 'Enter') searchValue = option;}}>{option}</button>
@@ -47,6 +96,17 @@
         {/if}
       {/each}
     </ul>
+    <div class="add-city">
+      <input 
+        bind:value={newCity}
+        type="text"
+        placeholder="Agregar nueva ciudad"
+        class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
+      >
+      <button class="ml-3 p-3 bg-green-800 rounded-full" on:click={addCity}>
+        Agregar
+      </button>
+    </div>
   </div>
 </div>
 
@@ -87,5 +147,9 @@
   }
   .dim {
     opacity: 0.5;
+  }
+  .add-city {
+    display: flex;
+    margin-top: 20px;
   }
 </style>
--- src/routes/cities/[slug]/+page.svelte
+++ src/routes/cities/[slug]/+page.svelte
@@ -1,183 +1,173 @@
-<!-- <script context="module">
-// @ts-ignore
-  export async function load({ page, fetch }) {
-    const { slug } = page.params;
-    // Aquí puedes cargar los datos del lugar basado en el slug
-    // Por ejemplo, podrías hacer una solicitud a una API
-    const res = await fetch(`api/url/${slug}`);
-    const data = await res.json();
+<script context="module">
+  import { goto } from '$app/navigation';
 
-    return {
-      props: {
-        place: data
-      }
-    };
+  export async function fetchData({ page, fetch }) {
+    const { slug } = page.params;
+
+    try {
+      const cityData = await fetchCityData(slug, fetch);
+      const eventsData = await fetchEventsData(fetch);
+
+      return {
+        props: {
+          city: cityData || {},
+          hotEvents: eventsData || []
+        }
+      };
+    } catch (error) {
+      console.error("Error loading data:", error);
+      return {
+        props: {
+          city: {},
+          hotEvents: []
+        }
+      };
+    }
   }
-</script> -->
+
+  async function fetchCityData(slug, fetch) {
+    const response = await fetch(`http://52.2.71.125:8000/cities/${slug}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch city data');
+    }
+    return await response.json();
+  }
+
+  async function fetchEventsData(fetch) {
+    const response = await fetch(`http://52.2.71.125:8000/events/`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch events data');
+    }
+    return await response.json();
+  }
+</script>
 
 <svelte:head>
-	<title>Cities</title>
-	<meta name="description" content="Cities" />
+  <title>Cities</title>
+  <meta name="description" content="Cities" />
 </svelte:head>
 
 <script>
   import { fly } from 'svelte/transition';
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
 
-  const city = {
-    name: 'Santiago',
-    country: 'Chile',
-    description: 'Descripción del lugar',
-    population: 7,
-    sectors: 32,
-    area: 641,
-    hills: 26,
-    parks: 6,
-    museums: 12,
-  };
+  export let city = {};
+  export let hotEvents = [];
 
-  let hot_events = [
-    { name: 'Evento 1', description: 'Descripción del evento 1', sector: 'Sector 1' },
-    { name: 'Evento 2', description: 'Descripción del evento 2', sector: 'Sector 2' },
-    { name: 'Evento 3', description: 'Descripción del evento 3', sector: 'Sector 3' }
-  ];
+  onMount(async () => {
+    // Aquí puedes añadir lógica que se ejecuta cuando el componente se monta
+  });
 </script>
 
 <div class="main-container">
-  <h1 class="mb-10 pl-10">{city.name}</h1>
-  <div class="main-info flex flex-col md:flex-row">
-    <div class="px-10 py-16 mx-auto sm:max-w-xl md:max-w-full">
-      <div class="grid gap-24 row-gap-8 lg:grid-cols-5">
-        <div class="grid gap-8 lg:col-span-2" in:fly={{ x:-100, duration:1000 }}>
-          <div>
-            <p class="mb-2 text-lg font-bold text-green-500">Al aire libre</p>
-            <p class="text-gray-700">
-              Santiago es una ciudad que se caracteriza por tener una gran cantidad de parques y cerros, los cuales son ideales para disfrutar de un día al aire libre.
-            </p>
-          </div>
-          <div>
-            <p class="mb-2 text-lg font-bold text-green-500">Curiosidad</p>
-            <p class="text-green-700">
-              Sabías que Santiago es la capital de Chile y una de las ciudades más grandes de América Latina.
-            </p>
-          </div>
-        </div>
-        <div class="grid border divide-y rounded lg:col-span-3 sm:grid-cols-2 sm:divide-y-0 sm:divide-x" in:fly={{ x:100, duration:1000, delay:500 }}>
-          <div class="flex flex-col justify-between p-10">
+  {#if city && city.name}
+    <h1 class="mb-10 pl-10">{city.name}</h1>
+    <div class="main-info flex flex-col md:flex-row">
+      <div class="px-10 py-16 mx-auto sm:max-w-xl md:max-w-full">
+        <div class="grid gap-24 row-gap-8 lg:grid-cols-5">
+          <div class="grid gap-8 lg:col-span-2" in:fly={{ x: -100, duration: 1000 }}>
             <div>
-              <p class="text-lg font-semibold text-green-600 sm:text-base">
-                Población 
-              </p>
-              <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                {city.population} M
-              </p>
+              <p class="mb-2 text-lg font-bold text-green-500">Al aire libre</p>
+              <p class="text-gray-700">{city.description}</p>
             </div>
             <div>
-              <p class="text-lg font-semibold text-green-600 sm:text-base">
-                Comunas
-              </p>
-              <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                {city.sectors}
-              </p>
-            </div>
-            <div>
-              <p class="text-lg font-semibold text-green-600 sm:text-base">
-                Superficie
-              </p>
-              <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                {city.area} km²
+              <p class="mb-2 text-lg font-bold text-green-500">Curiosidad</p>
+              <p class="text-green-700">
+                Sabías que {city.name} es la capital de {city.country} y una de las ciudades más grandes de América Latina.
               </p>
             </div>
           </div>
-          <div class="flex flex-col justify-between p-10">
-            <div>
-              <p class="text-lg font-semibold text-green-600 sm:text-base">Cerros</p>
-              <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                {city.hills}
-              </p>
+          <div class="grid border divide-y rounded lg:col-span-3 sm:grid-cols-2 sm:divide-y-0 sm:divide-x" in:fly={{ x: 100, duration: 1000, delay: 500 }}>
+            <div class="flex flex-col justify-between p-10">
+              <div>
+                <p class="text-lg font-semibold text-green-600 sm:text-base">Población</p>
+                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">{city.population} M</p>
+              </div>
+              <div>
+                <p class="text-lg font-semibold text-green-600 sm:text-base">Comunas</p>
+                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">{city.sectors}</p>
+              </div>
+              <div>
+                <p class="text-lg font-semibold text-green-600 sm:text-base">Superficie</p>
+                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">{city.area} km²</p>
+              </div>
             </div>
-            <div>
-              <p class="text-lg font-semibold text-green-600 sm:text-base">
-                Parques
-              </p>
-              <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                {city.parks}
-              </p>
-            </div>
-            <div>
-              <p class="text-lg font-semibold text-green-600 sm:text-base">
-                Museos
-              </p>
-              <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">
-                {city.museums}
-              </p>
+            <div class="flex flex-col justify-between p-10">
+              <div>
+                <p class="text-lg font-semibold text-green-600 sm:text-base">Cerros</p>
+                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">{city.hills}</p>
+              </div>
+              <div>
+                <p class="text-lg font-semibold text-green-600 sm:text-base">Parques</p>
+                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">{city.parks}</p>
+              </div>
+              <div>
+                <p class="text-lg font-semibold text-green-600 sm:text-base">Museos</p>
+                <p class="text-2xl font-bold text-deep-purple-accent-400 sm:text-xl">{city.museums}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  {:else}
+    <p>Loading...</p>
+  {/if}
   <div class="hot-trends w-full md:w-auto">
-    <div class="hot-events" in:fly={{ delay:500, duration:1000, y:100 }}>
+    <div class="hot-events" in:fly={{ delay: 500, duration: 1000, y: 100 }}>
       <h1 class="text-xl w-full">Lo más buscado</h1>
       <div class="space-y-6 border-l-2 border-dashed">
-        {#each hot_events as event}
+        {#each hotEvents as event}
           <div class="relative w-full hover:bg-gray-100">
-            <a href="/events/{event.name}">
+            <a href="/events/{event.id}">
               <div class="ml-6">
                 <h4 class="font-bold text-green-500">{event.name}</h4>
                 <p class="mt-2 max-w-screen-sm text-sm text-gray-500">{event.description}</p>
-                <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">
-                  {event.sector}
-                </span>
+                <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">{event.sector}</span>
               </div>
             </a>
           </div>
         {/each}
       </div>
     </div>
-    <div class="park-events" in:fly={{ delay:1000, duration:1000, y:100 }}>
+    <div class="park-events" in:fly={{ delay: 1000, duration: 1000, y: 100 }}>
       <h1 class="text-xl w-full">Parques</h1>
       <div class="space-y-6 border-l-2 border-dashed">
-        {#each hot_events as event}
+        {#each hotEvents as event}
           <div class="relative w-full hover:bg-gray-100">
             <div class="ml-6">
               <h4 class="font-bold text-green-500">{event.name}</h4>
               <p class="mt-2 max-w-screen-sm text-sm text-gray-500">{event.description}</p>
-              <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">
-                {event.sector}
-              </span>
+              <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">{event.sector}</span>
             </div>
           </div>
         {/each}
       </div>
     </div>
-    <div class="hill-events" in:fly={{ delay:1000, duration:1000, y:100 }}>
+    <div class="hill-events" in:fly={{ delay: 1000, duration: 1000, y: 100 }}>
       <h1 class="text-xl w-full">Cerros</h1>
       <div class="space-y-6 border-l-2 border-dashed">
-        {#each hot_events as event}
+        {#each hotEvents as event}
           <div class="relative w-full hover:bg-gray-100">
             <div class="ml-6">
               <h4 class="font-bold text-green-500">{event.name}</h4>
               <p class="mt-2 max-w-screen-sm text-sm text-gray-500">{event.description}</p>
-              <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">
-                {event.sector}
-              </span>
+              <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">{event.sector}</span>
             </div>
           </div>
         {/each}
       </div>
     </div>
-    <div class="all-events" in:fly={{ delay:1000, duration:1000, y:100 }}>
+    <div class="all-events" in:fly={{ delay: 1000, duration: 1000, y: 100 }}>
       <h1 class="text-xl w-full">Todos los eventos</h1>
       <div class="space-y-6 border-l-2 border-dashed">
-        {#each hot_events as event}
+        {#each hotEvents as event}
           <div class="relative w-full hover:bg-gray-100">
             <div class="ml-6">
               <h4 class="font-bold text-green-500">{event.name}</h4>
               <p class="mt-2 max-w-screen-sm text-sm text-gray-500">{event.description}</p>
-              <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">
-                {event.sector}
-              </span>
+              <span class="inline-flex items-center mt-2 px-3 py-2 rounded-full text-xs bg-green-100 text-green-400">{event.sector}</span>
             </div>
           </div>
         {/each}
@@ -185,7 +175,6 @@
     </div>
   </div>
 </div>
-
 <style>
   .video {
     display: flex;
@@ -212,31 +201,18 @@
     justify-content: space-evenly;
     width: 100%;
   }
-  .city-description {
-    display: flex;
-    flex-direction: column;
-    padding: 0 20px;
-  }
   .hot-trends {
     display: flex;
-    justify-content: space-evenly;
-    padding: 0 20px;
-    margin: 2% 0 15% 0;
+    flex-direction: row;
+    flex-wrap: wrap;
+    padding-left: 10%;
+    padding-right: 10%;
   }
-  .hot-events .park-events .hill-events .all-events {
+  .hot-events, .park-events, .hill-events, .all-events {
     display: flex;
     flex-direction: column;
-    padding: 0 20px;
-  }
-  @media (min-width: 1200px) {
-    .city-info {
-      width: 50%;
-    }
-    .hot-trends {
-      margin: 2% 0 4% 0;
-    }
-    .video {
-      margin: 4% 0;
-    }
+    padding-left: 5%;
+    padding-right: 5%;
+    width: 100%;
   }
 </style>
